fab12b99eb6b11172d240ff8caf4604dc28bc0b7
sms533 <sms533@georgetown.edu>
2021-09-16 12:06:23 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,17 @@
 # game.py
 
+import random # load the module to avoid `NameError: name 'random' is not defined
+
 print("Rock, Paper, Scissors, Shoot!")
 
 # prompt user for input
 
 user_choice = input("Choose 'rock' or 'paper' or 'scissors'")
 print(user_choice)
+
+# computer choice at random
+
+options = ["rock", "paper", "scissors"]
+
+computer_choice = random.choice(options)
+print(computer_choice)
